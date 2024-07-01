3e58e87e72cdd757cf34067ab4e3790e09974974
Leo Ying <leoleoying@gmail.com>
2024-07-01 17:45:45 +0900

    Fixed text error

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const inputImage = path.join(__dirname, '/cookies.jpg');
 const outputImage = path.join(__dirname, '/cookies_new.jpg');
-const captionText = "Sweet treats: Homemade cookies fresh from the oven";
+const captionText = "Sweet treats\\: Homemade cookies fresh from the oven";
 const fontPath = path.join(__dirname, '/fonts/Lobster/Lobster-Regular.ttf');
 
 ffmpeg(inputImage)
   .outputOptions([
-    '-vf', `drawtext=text='${captionText}':fontcolor=black:fontsize=200:fontfile=${fontPath}:x=(w-text_w)/2:y=(h-text_h)/4`,
+    '-vf', `drawtext=text='${captionText}':fontcolor=black:fontsize=150:fontfile=${fontPath}:x=(w-text_w)/2:y=(h-text_h)/4`,
     '-y'
   ])
   .save(outputImage)
