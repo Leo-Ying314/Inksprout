0f6fdda884b28b52e93dfd03d4f967c0cc8e1cf4
Leo Ying <leoleoying@gmail.com>
2024-07-05 17:02:02 +0900

    [Error] Add direct stream from request

--- thumbnail_API/index.js
+++ thumbnail_API/index.js
@@ -7,8 +7,8 @@ app.use(express.json()); // middleware to parse requests into JSON. Not necessar
 const PORT = 8080;
 
 // text alignment and font size values
-const text_align = ["start", "middle", "end"];
-const font_size = {
+const textAlignValues = ["start", "middle", "end"];
+const fontSizeValues = {
   small_extra: 150,
   small: 200,
   medium: 250,
@@ -17,37 +17,45 @@ const font_size = {
 };
 
 app.post("/thumbnail", async (req, res) => {
-  // post request which utilzies multer middleware to upload file
-  try {
-    // try - catch block for error handling
-    const { text, textalign = "middle", fontsize = "medium" } = req.body; // default text alignment and font size
+  let chunks = [];
+  req.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
 
-    // error handling
-    if (!text_align.includes(textalign)) {
-      return res.status(400).send({ error: "Invalid text alignment" });
-    }
+  req.on("error", (err) => {
+    console.log(err);
+  });
 
-    if (!font_size[fontsize]) {
-      return res.status(400).send({ error: "Invalid font size" });
-    }
+  req.on("end", async () => {
+    try {
+      const buffer = Buffer.concat(chunks);
 
-    if (text.length > 100) {
-      return res.status(400).send("Text is too long");
-    }
+      const image = sharp(buffer);
+      const metaData = await image.metadata();
 
-    if (/[^\w\s.,!?-]/.test(text)) {
-      return res
-        .status(400)
-        .send({ error: "Text contains unsupported characters" });
-    }
+      const { text, textAlignment = "middle", fontSize = "medium" } = req.body; // default text alignment and font size
 
-    // image processing utilizing sharp
+      // error handling
+      if (!textAlignValues.includes(textAlignment)) {
+        return res.status(400).send({ error: "Invalid text alignment" });
+      }
 
-    const inputStream = fs.createReadStream(req.file.path);
-    const outputStream = new PassThrough();
+      if (!fontSizeValues[fontSize]) {
+        return res.status(400).send({ error: "Invalid font size" });
+      }
 
-    const svgText = `
-      <svg width="500" height="500">
+      if (text.length > 100) {
+        return res.status(400).send("Text is too long");
+      }
+
+      if (/[^\w\s.,!?-]/.test(text)) {
+        return res
+          .status(400)
+          .send({ error: "Text contains unsupported characters" });
+      }
+
+      const svgText = `
+      <svg width="${metaData.width}" height="${metaData.height}">
         <style>
           .title { fill: black; font-size: ${font_size[fontsize]}px; font-family: Arial, sans-serif; }
         </style>
@@ -55,32 +63,24 @@ app.post("/thumbnail", async (req, res) => {
       </svg>
     `;
 
-    const transformStream = sharp()
-      .composite([
-        {
-          input: Buffer.from(svgText),
-        },
-      ])
-      .toFormat("png");
+      const outputBuffer = await image
+        .composite([
+          {
+            input: Buffer.from(svgText),
+          },
+        ])
+        .toFormat("png")
+        //.toBuffer();
 
-    inputStream.pipe(transformStream).pipe(outputStream);
-
-    let chunks = [];
-    outputStream.on("data", (chunk) => {
-      chunks.push(chunk);
-    });
-
-    outputStream.on("end", () => {
-      const buffer = Buffer.concat(chunks);
       res.status(200).json({
-        message: "Image uploaded successfully.",
-        data: buffer.toString("base64"),
+        message: "Image uploaded successfully",
+        data: outputBuffer.toString("base64"),
       });
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
+    } catch {
+      console.error(err);
+      res.status(500).send("Server error");
+    }
+  });
 });
 
 app.listen(PORT, () => {
